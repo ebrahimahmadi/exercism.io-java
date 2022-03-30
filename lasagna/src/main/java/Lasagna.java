f87451e9980e68da2db2df13371d3ba9646d79da
Ebi Ahmadi <eahmadi641@gmail.com>
2022-03-30 08:02:48 +0430

    feat(lasagna): add solution

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,16 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
-
-    // TODO: define the 'remainingMinutesInOven()' method
-
-    // TODO: define the 'preparationTimeInMinutes()' method
-
-    // TODO: define the 'totalTimeInMinutes()' method
+    private final int EXPECTED_COOKING_TIME = 40;
+    private final int LAYER_EXTRA_TIME = 2;
+    public int expectedMinutesInOven(){
+        return EXPECTED_COOKING_TIME;
+    }
+    public int remainingMinutesInOven(int elapsedMinutes){
+        return EXPECTED_COOKING_TIME - elapsedMinutes;
+    }
+    public int preparationTimeInMinutes(int layers){
+        return layers * LAYER_EXTRA_TIME;
+    }
+    public int totalTimeInMinutes(int layers, int elapsedMinutes){
+        return preparationTimeInMinutes(layers) + elapsedMinutes;
+    }
 }
